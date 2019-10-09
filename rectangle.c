df8aea02d4dacf7f4584f632da83072d1909ed07
chakravaram.saitejaswi <50818486+05saitejaswi@users.noreply.github.com>
2019-10-09 23:21:10 +0530

    rectangle in c
    creating some shapes like rectangle

---
+++ rectangle.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+Void main()
+{
+int i,j,n;
+clrscr();
+printf("enter the number of rows");
+scanf("%d",&n);
+for(i=1;i<=n;i++);
+{
+for(j=1;j<=n;j++)
+{
+if(i==1 || i==n || j==1 || j==n)
+{
+printf("*");
+}
+else
+{
+printf(" ");
+}
+}
+printf("\n");
+
+getch();
+}
